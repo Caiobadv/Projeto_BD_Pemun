3d425dd2f3238de7eacd9af33a9d9e2b2098a8da
Caiobadv <caiobarretodv@hotmail.com>
2024-05-22 19:35:21 -0300

    criando os arquivos

---
+++ src/main/java/com/example/Pemun_project/PemunProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pemun_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PemunProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PemunProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pemun_project/PemunProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pemun_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PemunProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
