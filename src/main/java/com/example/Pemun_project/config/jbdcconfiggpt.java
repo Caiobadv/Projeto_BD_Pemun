d95d05b3f17db8ed091c19587a23154143299a50
Caiobadv <caiobarretodv@hotmail.com>
2024-05-23 20:53:39 -0300

    remocao dos arquivos para test de entendimento

--- src/main/java/com/example/Pemun_project/config/jbdcconfiggpt.java
+++ src/main/java/com/example/Pemun_project/config/jbdcconfiggpt.java
@@ -10,20 +10,19 @@ import javax.sql.DataSource;
 @Configuration
 public class jbdcconfiggpt {
 
-        @Bean
-        public DataSource dataSource() {
-            DriverManagerDataSource dataSource = new DriverManagerDataSource();
-            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-            dataSource.setUrl("jdbc:mysql://localhost:3306/seu_banco_de_dados");
-            dataSource.setUsername("root");
-            dataSource.setPassword("Caiodavi1!");
-            return dataSource;
-        }
-
-        @Bean
-        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-            return new JdbcTemplate(dataSource);
-        }
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/seu_banco_de_dados");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Caiodavi1!");
+        return dataSource;
     }
 
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
 
---
+++ src/main/java/com/example/Pemun_project/service/UsuarioService.java
@@ -0,0 +1,1 @@
+package com.example.Pemun_project.service;
