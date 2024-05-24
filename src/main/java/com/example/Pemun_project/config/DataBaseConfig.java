b05f4dce520b7f6c980d0429c64f4ef68d3fe0bb
anabxalves <anabxalves@gmail.com>
2024-05-24 15:29:57 -0300

    go new gitignore2

--- src/main/java/com/example/Pemun_project/config/DataBaseConfig.java
+++ src/main/java/com/example/Pemun_project/config/DataBaseConfig.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DataBaseConfig {
 
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/PEMUN_DBPROJECT?useSSL=false&serverTimezone=UTC", "root", "Caiodavi1!");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/PEMUN_DBPROJECT?useSSL=false&serverTimezone=UTC", "root", "030701Ab");
     }
 
 
