c662141d8809591c2080a955b5873174aaaeacbb
Caiobadv <caiobarretodv@hotmail.com>
2024-05-24 13:14:28 -0300

    correcao do arquivo pom.xml, ajeitando a dependencia do nombok

--- src/main/java/com/example/Pemun_project/model/Usuario.java
+++ src/main/java/com/example/Pemun_project/model/Usuario.java
@@ -1,1 +1,23 @@
 package com.example.Pemun_project.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario {
+    private Integer cpf_user;
+    private String tipo_user;
+    private String nome_user;
+    private String instituicao_ensino_user;
+    private String curso_user;
+    private String login_user;
+    private String senha_login_user;
+    private String email_user;
+    private String telefone_user;
+    private String rua_endereco_user;
+    private String numero_endereco_user;
+    private String bairro_endereco_user;
+    private Integer cep_endereco_user;
+    private String complemento_endereco_user;
+}
