402e24b9d6f463aa6d268cd5c3da370ef3678a38
Virna Amaral <vfpa@cesar.school>
2024-05-25 19:59:45 -0300

    alterações pequenas nos arquivos

--- front_db_pemun/src/pages/comiteunche.js
+++ front_db_pemun/src/pages/comiteunche.js
@@ -147,7 +147,7 @@ function ComiteUnche() {
                 </div>
                 
                 <div className="footer-credits">
-                    <p>Designed and Developed by Ana Beatriz Alves e Caio Barreto</p>
+                    <p>Designed and Developed by Ana Beatriz Alves, Caio Barreto e Virna Amaral</p>
                     <p className="cor-verde">© Copyright 2024, All Rights Reserved by PEMUN</p>
                 </div>
                 </div>
