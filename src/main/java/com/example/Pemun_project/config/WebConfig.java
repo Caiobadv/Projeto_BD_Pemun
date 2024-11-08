a5e4b018e92edf9b764ee9cb0533d7a1536a7e44
Caio Barreto de Albuqueque <117755420+Caiobadv@users.noreply.github.com>
2024-11-08 17:00:31 -0300

    Atualizar o WebConfig.java

--- src/main/java/com/example/Pemun_project/config/WebConfig.java
+++ src/main/java/com/example/Pemun_project/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Substitua pela URL do seu frontend
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
